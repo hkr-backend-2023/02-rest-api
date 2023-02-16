5cbdd8cf4c64fc38f0222533dd527e6c400c4b1c
David Andersson <david.andersson@zocom.se>
2023-02-16 15:42:57 +0100

    Serving static files

--- src/server.js
+++ src/server.js
@@ -6,9 +6,12 @@ const movieRouter = require('./routes/movies')
 // Setup the server
 const port = 1337
 const app = express()
+const staticPath = 'src/static'
+// use __dirname and path.join if this path does not work
 
 
 // Middleware
+app.use( express.static(staticPath) )
 app.use( express.json() )
 
 
@@ -16,7 +19,6 @@ app.use( express.json() )
 // Register routes
 app.use('/api/movies', movieRouter)
 // app.use('/api/actors', actorRouter)  <- if we need to add more endpoints
-
 
 
 // Start the server
